8f3efc34e9b8492964e866f88ce64fbab0dbc662
Jacob Steel <140410193+St33li0@users.noreply.github.com>
2026-03-12 21:50:37 +0000

    Update main.py
    Fixed mod file list creation.
    Todo: Find better implementation other than os.walk

--- main.py
+++ main.py
@@ -11,10 +11,10 @@ parser = argparse.ArgumentParser(
 					description = 'Config Based Modpack Manager for SinglePlayer Tarkov',
 					epilog = 'Are you ready to die?')
 parser.suggest_on_error = True
-parser.add_argument("-i","--sptdir",default="C:\\SPT")
-parser.add_argument("-s","--stagingdir",default="C:\\SPT\\SPT_CBMM_Staging")
-parser.add_argument("-u","--url",default="https://github.com/St33li0/sptarkov_configs")
-parser.add_argument("-f","--force",default=False,action="store_true")
+parser.add_argument("-i","--sptdir",default="C:\\SPT",help="Directory of SPT Installation. Defaults to C:\\SPT")
+parser.add_argument("-s","--stagingdir",default="C:\\SPT\\SPT_CBMM_Staging",help="Directory to stage mods and store backups. Default = '$SPTDIR\\SPT_CBMM_Staging'")
+parser.add_argument("-u","--url",default="https://github.com/St33li0/sptarkov_configs",help="Best not to change this unless you know what you are doing.")
+parser.add_argument("-f","--force",default=False,action="store_true",help="Does not prompt user for confirmation when enabled.")
 
 # def get_arg_dict(args):
 # 	return {
@@ -26,6 +26,10 @@ parser.add_argument("-f","--force",default=False,action="store_true")
 # args = get_arg_dict(parser.parse_args())
 
 parsed = parser.parse_args()
+
+if parsed.sptdir != "C:\\SPT" and parsed.stagingdir == "C:\\SPT\\SPT_CBMM_Staging":
+	parsed.stagingdir = (f"{parsed.sptdir}\\SPT_CBMM_Staging")
+
 args = {
 	"spt_install_dir": parsed.sptdir,
 	"mod_staging_dir": parsed.stagingdir,
@@ -49,26 +53,29 @@ SPT_MODS_FOLDER = {
 	"SPT": {"root":(f"{args["spt_install_dir"]}\\SPT\\user\\mods"),"files":[]}
 }
 SPT_CONFIGS_FOLDER = {
-	"BepInEx": {"root":(f"{args["spt_install_dir"]}\\Bepinex\\configs"),"files":[]},
+	"BepInEx": {"root":(f"{args["spt_install_dir"]}\\Bepinex\\config"),"files":[]},
 	"SPT": {"root":(f"{args["spt_install_dir"]}\\SPT\\user\\configs"),"files":[]}
 }
 
 # Walk directory to get mods
-for r,d in os.walk(SPT_MODS_FOLDER["BepInEx"]["root"]):
+for r,d,_f in os.walk(SPT_MODS_FOLDER["BepInEx"]["root"]):
+	if r != SPT_MODS_FOLDER["BepInEx"]["root"]: continue
 	SPT_MODS_FOLDER["BepInEx"]["files"].append(d)
-for r,d in os.walk(SPT_CONFIGS_FOLDER["BepInEx"]["root"]):
+for r,d,_f in os.walk(SPT_CONFIGS_FOLDER["BepInEx"]["root"]):
+	if r != SPT_MODS_FOLDER["BepInEx"]["root"]: continue
 	SPT_CONFIGS_FOLDER["BepInEx"]["files"].append(d)
-for r,d in os.walk(SPT_MODS_FOLDER["SPT"]["root"]):
+for r,d,_f in os.walk(SPT_MODS_FOLDER["SPT"]["root"]):
+	if r != SPT_MODS_FOLDER["BepInEx"]["root"]: continue
 	SPT_MODS_FOLDER["SPT"]["files"].append(d)
-for r,d in os.walk(SPT_CONFIGS_FOLDER["SPT"]["root"]):
+for r,d,_f in os.walk(SPT_CONFIGS_FOLDER["SPT"]["root"]):
+	if r != SPT_MODS_FOLDER["BepInEx"]["root"]: continue
 	SPT_CONFIGS_FOLDER["SPT"]["files"].append(d)
 
 # Walk folders to get dict of mods and their files
 mods = {}
 
 for file in SPT_MODS_FOLDER["BepInEx"]["files"]:
-	for r,d,f in os.walk(file):
-		print("File",file,r,d,f)
-	
+	pass
+
 for mod in mods:
 	pass
